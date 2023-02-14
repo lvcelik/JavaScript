9a0a0e3cbc171e4cc7545d9ebfd2ffa96f65f712
Lovro Vcelik <lvcelik8@gmail.com>
2023-02-14 19:03:07 +0100

    Promjena JavaScript

---
+++ Leksička struktura 2/pocetno/varijable.js
@@ -0,0 +1,22 @@
+var firstName, lastName, price, discount, fullPrice;
+
+firstName = "John";
+lastName = "Doe";
+price = 19.90;
+discount = 0.10;
+
+var x = 3;
+x = 1;
+if (x == 1){
+x = 2;
+}
+
+var a = true;
+if (a == true) {
+    var z = 44;
+    return 0;
+    }
+
+
+fullPrice = (price * 100) / discount;               
+
