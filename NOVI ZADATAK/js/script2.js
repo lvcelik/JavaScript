4e0bec2b27458ebe7074be2c0ec608989cc9da90
Lovro Vcelik <lvcelik8@gmail.com>
2023-02-16 19:04:40 +0100

    Promjena 16.2.2023. Algebra Java Script

---
+++ NOVI ZADATAK/js/script2.js
@@ -0,0 +1,27 @@
+var text = " Loler ipsum dolor sit amet";
+
+var length = text.length
+
+
+
+var wordSit = text.substr(19, 3);
+var wordSit2 = text.substring(19, 19 + 3);
+
+
+
+var text2 = text.replace("amet", "elit");
+
+var newText = text + ", consectetur adipiscing elit.";
+
+
+text = text.trim();
+console.log(text);
+
+
+
+
+
+
+
+
+
