f5234f8f7fa92071632c1ecac4796eef17d06e05
Lovro Vcelik <lvcelik8@gmail.com>
2023-02-22 19:26:58 +0100

    Promjena druga 22.2.2023.

--- 22.2.2023 Funkcije/zadatakPredavanje.js
+++ 22.2.2023 Funkcije/zadatakPredavanje.js
@@ -1,4 +1,4 @@
-function trazInajduziRijec(zadaniNiz){
+/*function trazInajduziRijec(zadaniNiz){
     var najdzaRijecUFunkciji = "";
     for(let i = 0; i < zadaniNiz.length; i++){
         if (zadaniNiz[i].length > najdzaRijecUFunkciji.length){
@@ -9,7 +9,29 @@ function trazInajduziRijec(zadaniNiz){
     
     
     return najdzaRijecUFunkciji;
+}*/
+
+function checkEmail(email){
+    var i = email.indexOf("@");
+    console.log("search index i = " + i);
+    if(i > 0){
+        var tmpEmailDomain = email.substring(i+1, email.length);
+        console.log(tmpEmailDomain)
+        var z = tmpEmailDomain.indexOf (".");
+        console.log("searc index z = " + z);
+        if (z > 0){
+            return true;
+        }
+        return false;
+    }else{
+    return false;
+    }
 }
+
+var isEmailOk = checkEmail("moje.ime@email.com");
+console.log("email ok = " + isEmailOk);
+
+return
 
 var niz = ["Marko", "Ana", "Josipa", "Anamarija", "Ivo"];
 
