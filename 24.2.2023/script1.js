910b89b0ba1f66ddeb9bf74d8d4214da3951e23d
Lovro Vcelik <lvcelik8@gmail.com>
2023-02-24 19:19:30 +0100

    Promjena 24.2.2023.

---
+++ 24.2.2023/script1.js
@@ -0,0 +1,21 @@
+var d2 = new Date();
+console.log(new Date);
+
+
+var d = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(new Date(2018, 11, 24, 10, 33, 30, 0));
+console.log(d2.getFullYear());
+
+
+var d = new Date("October 13, 2014 11:13:00");
+console.log(new Date("October 13, 2014 11:13:00"));
+
+
+var d = new Date(-100000000000);
+console.log(new Date(-100000000000));
+
+return;
+
+
+
+
