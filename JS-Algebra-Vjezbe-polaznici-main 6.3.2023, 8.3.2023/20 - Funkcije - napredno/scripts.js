8b09d65fa0dccacc48b8db2064c6a10832a04e8b
Lovro Vcelik <lvcelik8@gmail.com>
2023-03-08 19:24:43 +0100

    Promjena 8.3.2023.

---
+++ JS-Algebra-Vjezbe-polaznici-main 6.3.2023, 8.3.2023/20 - Funkcije - napredno/scripts.js
@@ -0,0 +1,14 @@
+// 1.
+var x = "John Doe"; 
+function vratiNesto(name) {
+   var x = name;
+   function f() { return x; }
+   return f;
+}
+var y = vratiNesto("Jane Doe");
+var z = vratiNesto()();
+
+
+
+
+
---
+++ JS-Algebra-Vjezbe-polaznici-main 6.3.2023, 8.3.2023/script.js
@@ -0,0 +1,23 @@
+// function vratiNesto(x){
+//     return x;
+// }
+// var y = vratiNesto();
+
+// // console.log(y)
+
+
+// function vratiNesto(x){
+//     return x;
+// }
+
+// var y = vratiNesto;
+// y("Hello world");
+
+// // console.log()
+
+var person = {
+    name: `John`,
+    surname: `Doe`,
+};
+
+console.log(person);
