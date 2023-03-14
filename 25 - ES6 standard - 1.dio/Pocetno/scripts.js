d04cc388b2750bff7b327f05c77f797b0f516e49
Lovro Vcelik <lvcelik8@gmail.com>
2023-03-14 19:17:54 +0100

    Promjena 14.3.2023. drugi put

--- 25 - ES6 standard - 1.dio/Pocetno/scripts.js
+++ 25 - ES6 standard - 1.dio/Pocetno/scripts.js
@@ -1,45 +1,60 @@
-'use strict';
-// 1.
-var a = 78;
-var b = 34;
-//delete a;
-//delete window.b;
-var myObject = {property1:1, property2:2, property1:1};
+// 'use strict';
+// // 1.
+// var a = 78;
+// var b = 34;
+// //delete a;
+// //delete window.b;
+// var myObject = {property1:1, property2:2, property1:1};
 
-// 2.
-const users = [
-  { name: 'Fabio', surname: 'Biondi' },
-  { name: 'Mario', surname: 'Rossi' },
-];
+// // 2.
+// const users = [
+//   { name: 'Fabio', surname: 'Biondi' },
+//   { name: 'Mario', surname: 'Rossi' },
+// ];
 
-const user = { name: 'Lorenzo', surname: 'Verdi' };
-var noviUseri = [...users,user]; 
+// const user = { name: 'Lorenzo', surname: 'Verdi' };
+// var noviUseri = [...users,user]; 
 
-// console.log(noviUseri[2].name);
-// for(let i = 0; i<noviUseri.length; i++){  //prvi način
-//   console.log(noviUseri[i].name);
+// // console.log(noviUseri[2].name);
+// // for(let i = 0; i<noviUseri.length; i++){  //prvi način
+// //   console.log(noviUseri[i].name);
+// // }
+// // console.log("----------");
+
+
+// // noviUseri.forEach(function(item){         //drugi način
+// //   console.log(item.name);
+// // });
+// // console.log("----------");
+
+// // for( let c in noviUseri){                 //treći način
+// //   console.log(noviUseri[c].name);
+// // }
+
+// // 3.
+// for (var i = 0; i < 10; i++) {
+//   console.log('The number is' + i);
+//  setTimeout(function() {
+//    console.log('The number is ' + i);
+//  }, 1000);
 // }
-// console.log("----------");
 
+// console.log(i);
 
-// noviUseri.forEach(function(item){         //drugi način
-//   console.log(item.name);
-// });
-// console.log("----------");
-
-// for( let c in noviUseri){                 //treći način
-//   console.log(noviUseri[c].name);
-// }
-
-
-
-
-// 3.
-for (var i = 0; i < 10; i++) {
-  console.log('The number is' + i);
- setTimeout(function() {
-   console.log('The number is ' + i);
- }, 1000);
+function getX() {
+  return Math.random() >= 0.5;
 }
 
-console.log(i);
+var wait = new Promise ((resolve, reject) => {
+  const x = getX();
+
+  if (x) {
+    return setTimeout(resolve, 1000);
+  }
+  return setTimeout(reject, 1000);
+},
+);
+.then(() => console.log ('Yay!'));
+.then(() => console.log ('Oh no!'));
+
+
